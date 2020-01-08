8fac2d8e93c9fcf0595fb7128fc473281fe1f81e
Jeremy Garnes <j06garnes@gmail.com>
2020-01-08 02:02:21 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -6,13 +6,13 @@ require 'pry'
 require 'pp'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-binding.pry 
+#binding.pry 
 pp nds 
 end
 
 
 def print_first_directors_movie_titles
-binding.pry 
+#binding.pry 
 
 i = 0 
 while i < directors_database[0][:movies].length do
