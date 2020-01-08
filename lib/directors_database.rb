c789afa28907211cecd29c4b499055ab237fcf98
Jeremy Garnes <j06garnes@gmail.com>
2020-01-08 01:50:51 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -2,7 +2,7 @@ require 'yaml'
 require 'pry'
 require 'pp'
 def directors_database
-binding.pry 
+#binding.pry 
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 
